4aa4e3078eb32f49c0ad13eedc4f34bf6707a858
sunny-7893320220 <163244207+sunny-7893320220@users.noreply.github.com>
2024-07-24 12:55:11 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sudo yum install git -y
-sudo yum install python-pip3 -y
-git clone https://github.com/sunny-7893320220/Medical-Insurance.git
-cd /
-sudo mv Medical-Insurance /home/ec2-user/
-cd /home/ec2-user/
-cd Medical-Insurance/
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+cd /home
+sudo git clone https://github.com/sunny-7893320220/Medical-Insurance.git
+cd /home/Medical-Insurance/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
