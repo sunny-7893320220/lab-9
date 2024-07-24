cd3d02da57607f3e52cf86ba56ef60b2a341aa72
sunny-7893320220 <163244207+sunny-7893320220@users.noreply.github.com>
2024-07-24 16:19:04 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ sudo yum install git -y
 sudo yum install python3 -y
 sudo yum install python3-pip -y
 cd /home
-sudo git clone https://github.com/Nayak-6666/python-car-prediction-1.git
-cd /home/python-car-prediction/
+sudo git clone https://github.com/sunny-7893320220/flight-perdiction.git
+cd /home/flight-perdiction/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
