b7dc3b3a4566c91f8418fb52aa84ece4a6af269b
sunny-7893320220 <163244207+sunny-7893320220@users.noreply.github.com>
2024-07-24 15:33:58 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ sudo yum install git -y
 sudo yum install python3 -y
 sudo yum install python3-pip -y
 cd /home
-sudo git clone https://github.com/sunny-7893320220/Medical-Insurance.git
+sudo git clone https://github.com/Nayak-6666/python-car-prediction-1.git
 cd /home/Medical-Insurance/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
